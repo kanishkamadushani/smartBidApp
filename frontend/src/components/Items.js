7d052ab7d132721cde6f303585004947d03f8f48
kanishkamadushani <kanishkam3956@gmail.com>
2025-04-11 11:41:53 +0300

    dash bord boder con

--- frontend/src/components/Items.js
+++ frontend/src/components/Items.js
@@ -25,20 +25,22 @@ const Items = () => {
   }, []);
 
   return (
-    <div className="container">
-      <div className="row row-cols-1 row-cols-md-2 g-4">
-        {items &&
-          items.map((item) => (
-            <Item
-              key={item._id}
-              name={item.name}
-              category={item.category}
-              amount={item.amount}
-              description={item.description}
-              img={item.img}
-              id={item._id}
-            />
-          ))}
+    <div className="album py-5 bg-body-tertiary">
+      <div className="container">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+          {items &&
+            items.map((item) => (
+              <Item
+                key={item._id}
+                name={item.name}
+                category={item.category}
+                amount={item.amount}
+                description={item.description}
+                img={item.img}
+                id={item._id}
+              />
+            ))}
+        </div>
       </div>
     </div>
   );
