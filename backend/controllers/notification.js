e0035b6e8222d73dac192c97385cbe65ea11d90e
kanishkamadushani <kanishkam3956@gmail.com>
2025-05-02 19:34:04 +0300

    get all notifications backend add

--- backend/controllers/notification.js
+++ backend/controllers/notification.js
@@ -1,4 +1,5 @@
 import { Notification } from "../models/notificationModel.js";
+import { User } from "../models/userModel.js";
 
 //create an notification
 export const createNotification = async (req, res) => {
@@ -11,6 +12,26 @@ export const createNotification = async (req, res) => {
       secondary_user_id,
     });
     res.status(200).json(notification);
+  } catch (error) {
+    res.status(401).json({ error: error.message });
+  }
+};
+
+//gell all notifications for an email (user)
+export const getNotification = async (req, res) => {
+  const { email } = req.params;
+  try {
+    //search user_id from email
+    const user = await User.findOne({ email }, { _id: 1 }).lean();
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const notifications = await Notification.find({
+      $or: [{ primary_user_id: user }, { secondary_user_id: user }],
+    });
+
+    res.status(200).json(notifications);
   } catch (error) {
     res.status(401).json({ error: error.message });
   }
--- backend/routes/notification.js
+++ backend/routes/notification.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { createNotification } from "../controllers/notification.js";
+import {
+  createNotification,
+  getNotification,
+} from "../controllers/notification.js";
 
 const router = express.Router();
 
 //create notification
 router.post("/create", createNotification);
+
+//get all notification for a user
+router.get("/:email", getNotification);
 
 export default router;
